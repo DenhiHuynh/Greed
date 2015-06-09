63c57f111fff341af94c9108ad293c0e71308782
Denhi Huynh <Denhi_@hotmail.com>
2015-06-09 14:14:23 +0200

    Added new project

---
+++ app/src/main/java/com/example/denhi/greed/Greed.java
@@ -0,0 +1,11 @@
+package com.example.denhi.greed;
+
+/**
+ * Created by Denhi on 2015-06-08.
+ */
+public class Greed {
+    public static int evaluateScore(){
+        //Check how many point a dice throw should give
+        return 0;
+    }
+}
